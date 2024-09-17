4044ba5b7f051ec89c79b9f3371db6c6989bea36
SamuelAraag <samoelgaaradossantos@gmail.com>
2024-09-16 22:10:00 -0300

    obtendo lista de filmes e jogando nomes em tela #time 20m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { Movie } from './models/movie.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  movies: Movie[] = [];
+  constructor(private http: HttpClient){}
+
+  async ngOnInit() {
+    const url = "https://ghibliapi.vercel.app/films";
+    this.movies = await lastValueFrom(this.http.get<Movie[]>(url));
+  }
   title = 'ghibli-memories';
 }
